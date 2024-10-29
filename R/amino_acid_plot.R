a0fc26e32b68a16e073ada56c2625c35ac6f3706
EmmChr <s204543@dtu.dk>
2024-10-29 11:09:31 +0100

    Updating code commenting

--- R/amino_acid_plot.R
+++ R/amino_acid_plot.R
@@ -15,16 +15,19 @@
 #' amino_acid_plot(entH)
 amino_acid_plot <- function(aa_sequence){
   aa_present <- aa_sequence |>
-    stringr::str_split(pattern = stringr::boundary("character"), simplify = TRUE) |>
-    as.character() |>
-    unique()
+    stringr::str_split(pattern = stringr::boundary("character"), simplify = TRUE) |> #Split strings for each character in a character matrix
+    as.character() |> #Ensure all is character
+    unique() #List of unique amino acids
 
+  # Count amino acid frequency in sequence
   counts <- sapply(aa_present, function(amino_acid) stringr::str_count(string = aa_sequence, pattern =  amino_acid)) |>
     as.data.frame()
 
+  # Rename column names and rownames
   colnames(counts) <- c("Counts")
   counts[["aa_sequence"]] <- rownames(counts)
 
+  # Plot the counts, coloured by the sequence
   aa_count_plot <- counts |>
     ggplot2::ggplot(ggplot2::aes(x = aa_sequence, y = Counts, fill = aa_sequence)) +
     ggplot2::geom_col() +
