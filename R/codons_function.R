73219ccb30f39f7476985f09bb873a84afcef7ca
HelenaMoller <s204585@student.dtu.dk>
2024-10-29 11:04:10 +0100

    merge s204585 branch with master branch

---
+++ R/codons_function.R
@@ -0,0 +1,19 @@
+#' Function three
+#'
+#' @param dna description of parameter
+#' @param start description of parameter
+#'
+#' @return description of function output
+#' @export
+#'
+#' @examples
+extract_codons <- function(sequence, start = 1){
+  lenght <- nchar(sequence)
+  codons <- substring(sequence,
+                      first = seq(from = start, to = lenght-3+1, by = 3),
+                      last = seq(from = 3+start-1, to = lenght, by = 3))
+  return(codons)
+}
+
+extract_codons("AGCTGACGTAG", start = 1)
+#[1] "AGC" "TGA" "CGT"
