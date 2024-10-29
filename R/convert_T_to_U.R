e4cf0510727dbd481b25011d137a8fd2447db922
FHolmstrup <freho@dtu.dk>
2024-10-29 11:31:25 +0100

    Hej, Jeg har implementeret function 2

---
+++ R/convert_T_to_U.R
@@ -0,0 +1,12 @@
+#' Convert DNA sequence to RNA sequence
+#'
+#' @param DNA_sequence a DNA sequence of nucleotides containing letters "A", "C", "G" and "T"
+#'
+#' @return a RNA sequence of nucleotides containing letters "A", "C", "G" and "U"
+#' @export
+#'
+#' @examples convert_T_to_U("ACGTTGCA")
+convert_T_to_U <- function(DNA_sequence){
+  RNA_sequence <- gsub("T", "U", DNA_sequence)
+  return(RNA_sequence)
+}
---
+++ tests/testthat/test-T_to_U_converter.R
@@ -0,0 +1,3 @@
+test_that("convertion of DNA to RNA works", {
+  expect_equal("ACGTTGCA", "ACGUUGCA")
+})
