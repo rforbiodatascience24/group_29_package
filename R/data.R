c79b067bf232f49230f368e9feea2a98d27ca5e3
FHolmstrup <freho@dtu.dk>
2024-10-29 09:39:27 +0100

    Hej, Her er en tabel med codons og tilhørende aminosyrer

---
+++ R/data.R
@@ -0,0 +1,7 @@
+#' codon_table data
+#'
+#' This table indicates which amino acids are encoded by the three bases (codons)
+#'
+#'
+#' @source \url{https://www.ncbi.nlm.nih.gov/Taxonomy/Utils/wprintgc.cgi?chapter=tgencodes#SG1}
+"codon_table"
