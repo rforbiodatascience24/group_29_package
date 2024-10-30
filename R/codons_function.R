2bd1b5d54685802b85d325185ec1990dde1c5838
HelenaMoller <s204585@student.dtu.dk>
2024-10-30 13:44:23 +0100

    opdateret vignette, test af extract_codons og funktion tre (extract_codons)

--- R/codons_function.R
+++ R/codons_function.R
@@ -1,17 +1,18 @@
 #' Function three
 #'
-#' @param dna description of parameter
-#' @param start description of parameter
+#' @param RNA_sequence gets extracted into codons of three nucleotide each
 #'
-#' @return description of function output
+#' @return strings of different codons each with three nucleotides from RNA sequence
 #' @export
 #'
 #' @examples
-#' extract_codons("AGCTGACGTAG", start = 1)
-extract_codons <- function(sequence, start = 1){
-  lenght <- nchar(sequence)
-  codons <- substring(sequence,
+#' RNA_sequence <- "ACGUUGCA"
+#' extract_codons(RNA_sequence)
+extract_codons <- function(RNA_sequence, start = 1){
+  lenght <- nchar(RNA_sequence)
+  codons <- substring(RNA_sequence,
                       first = seq(from = start, to = lenght-3+1, by = 3),
                       last = seq(from = 3+start-1, to = lenght, by = 3))
   return(codons)
 }
+
--- tests/testthat/test-extract_codons.R
+++ tests/testthat/test-extract_codons.R
@@ -1,7 +1,15 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("codon extraction works", {
+  codons <- extract_codons("ACGUUGCA")
+  expect_equal(codons, c("ACG", "UUG"))
 })
 
-test_that("subtraction works", {
-  expect_equal(10-0,10)
+
+test_that("Handles incomplete codon", {
+  codons <- extract_codons("ACGUUG")
+  expect_equal(codons, c("ACG", "UUG"))
+})
+
+test_that("Starting at different positions", {
+  codons <- extract_codons("ACGUUGCA", start = 2)
+  expect_equal(codons, c("CGU", "UGC"))
 })
